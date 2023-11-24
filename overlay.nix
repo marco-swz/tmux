5925586b3693ba1b5475f48fad81a2899ae0a109
xRetry <marcoschwarz.at@gmail.com>
2023-11-24 10:54:10 +0100

    Fix tmux colors

--- overlay.nix
+++ overlay.nix
@@ -11,7 +11,7 @@ final: prev: {
     };
 
     tmux-configured = let 
-        tmux-script = final.writeShellScriptBin "tmux" "${prev.tmux}/bin/tmux -f ${final.tmux-config}/tmux.conf";
+        tmux-script = final.writeShellScriptBin "tmux" "TERM=xterm-256color ${prev.tmux}/bin/tmux -f ${final.tmux-config}/tmux.conf";
     in final.stdenv.mkDerivation {
 
         name = "tmux-configured";
